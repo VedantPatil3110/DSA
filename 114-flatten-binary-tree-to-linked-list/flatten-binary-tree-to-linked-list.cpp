90f65c033675168d2708bb06526d28a3493677bc
Vedant Patil <157149251+badboy3110@users.noreply.github.com>
2025-02-08 09:22:14 +0530

    Time: 0 ms (100.00%) | Memory: 17.5 MB (77.09%) - LeetSync

--- 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.cpp
+++ 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.cpp
@@ -11,26 +11,41 @@
  */
 class Solution {
 public:
-void preorder(TreeNode*root,vector<TreeNode*>&ans){
-    if(root==NULL){
-        return;
-    }
-    ans.push_back(root);
-    preorder(root->left,ans);
-    preorder(root->right,ans);
-}
-void connection(vector<TreeNode*>ans,int i,int n){
-    while(i+1<=n){
-        ans[i]->right=ans[i+1];
-        ans[i]->left=NULL;
-        i++;
-    }
-}
+// void preorder(TreeNode*root,vector<TreeNode*>&ans){
+//     if(root==NULL){
+//         return;
+//     }
+//     ans.push_back(root);
+//     preorder(root->left,ans);
+//     preorder(root->right,ans);
+// }
+// void connection(vector<TreeNode*>ans,int i,int n){
+//     while(i+1<=n){
+//         ans[i]->right=ans[i+1];
+//         ans[i]->left=NULL;
+//         i++;
+//     }
+// }
     void flatten(TreeNode* root) {
-        vector<TreeNode*>ans;
-        preorder(root,ans);
-        int i=0;
-        int n=ans.size()-1;
-        connection(ans,i,n);
+        // vector<TreeNode*>ans;
+        // preorder(root,ans);
+        // int i=0;
+        // int n=ans.size()-1;
+        // connection(ans,i,n);
+        if(root==NULL){
+            return;
+        }
+        TreeNode*l=root->left;
+        TreeNode*r=root->right;
+        root->left=NULL;
+        root->right=NULL;
+        flatten(l);
+        flatten(r);
+        root->right=l;
+        TreeNode*temp=root;
+        while(temp->right!=NULL){
+            temp=temp->right;
+        }
+        temp->right=r;
     }
 };
