65cb415f98813900155ae1444c76d2af3c2dd305
Vedant Patil <157149251+badboy3110@users.noreply.github.com>
2025-02-21 14:58:11 +0530

    Time: 3 ms (55.41%) | Memory: 9.8 MB (26.17%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -5,24 +5,42 @@ public:
             return false;
         }
         unordered_map<char,int>m1;
-        unordered_map<char,int>m2;
+        // unordered_map<char,int>m2;
         for(int i=0;i<s.size();i++){
             m1[s[i]]++;
-            m2[t[i]]++;
+            // m2[t[i]]++;
         }
-        for(auto x:m1){
-            char ch1=x.first;
-            int freq1=x.second;
-            if(m2.find(ch1)!=m2.end()){
-                int freq2=m2[ch1];
-                if(freq1!=freq2){
-                    return false;
+        // for(auto x:m1){
+        //     char ch1=x.first;
+        //     int freq1=x.second;
+        //     if(m2.find(ch1)!=m2.end()){
+        //         int freq2=m2[ch1];
+        //         if(freq1!=freq2){
+        //             return false;
+        //         }
+        //     }
+        //     else{
+        //         return false;
+        //     }
+        // }
+        // return true;
+        for(int i=0;i<s.size();i++){
+            char ch=t[i];
+            if(m1.find(ch)!=m1.end()){
+                m1[ch]--;
+                if(m1[ch]==0){
+                    m1.erase(ch);
                 }
             }
             else{
                 return false;
             }
         }
-        return true;
+        if(m1.size()>0){
+            return false;
+        }
+        else{
+            return true;
+        }
     }
 };
