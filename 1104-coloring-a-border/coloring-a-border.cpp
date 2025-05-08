775ba997683ac3f920e0297a2cf5970b6931768a
Vedant Patil <157149251+VedantPatil3110@users.noreply.github.com>
2025-05-08 19:26:26 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (96.65%) - LeetSync

--- 1104-coloring-a-border/coloring-a-border.cpp
+++ 1104-coloring-a-border/coloring-a-border.cpp
@@ -1,30 +1,29 @@
 class Solution {
 public:
-vector<pair<int,int>>internal;
-void dfs(vector<vector<int>>&grid,int r,int c,int newcolor,int originalcolor){
-    if(r<0||c<0||r>=grid.size()||c>=grid[0].size()||grid[r][c]!=originalcolor){
+vector<pair<int,int>>v;
+void dfs(vector<vector<int>>&grid,int row,int col,int color,int original){
+    if(row<0||col<0||row>=grid.size()||col>=grid[0].size()||grid[row][col]!=original){
         return;
     }
-    grid[r][c]=-newcolor;
-    dfs(grid,r+1,c,newcolor,originalcolor);
-    dfs(grid,r,c+1,newcolor,originalcolor);
-    dfs(grid,r-1,c,newcolor,originalcolor);
-    dfs(grid,r,c-1,newcolor,originalcolor);
-
-    if(not (r==0||c==0||r==grid.size()-1||c==grid[0].size()-1||grid[r+1][c]!=-newcolor||grid[r-1][c]!=-newcolor||grid[r][c+1]!=-newcolor||grid[r][c-1]!=-newcolor)){
-        internal.push_back({r,c});
+    grid[row][col]=-color;
+    dfs(grid,row+1,col,color,original);
+    dfs(grid,row-1,col,color,original);
+    dfs(grid,row,col+1,color,original);
+    dfs(grid,row,col-1,color,original);
+    if(not (row==0||col==0||row==grid.size()-1||col==grid[0].size()-1||grid[row+1][col]!=-color||grid[row-1][col]!=-color||grid[row][col+1]!=-color||grid[row][col-1]!=-color)){
+        v.push_back({row,col});
     }
 }
     vector<vector<int>> colorBorder(vector<vector<int>>& grid, int row, int col, int color) {
-        int originalcolor=grid[row][col];
-        if(originalcolor==color){
+        if(grid[row][col]==color){
             return grid;
         }
-        dfs(grid,row,col,color,originalcolor);
-        for(auto p:internal){
-            int i=p.first;
-            int j=p.second;
-            grid[i][j]=originalcolor; 
+        int original=grid[row][col];
+        dfs(grid,row,col,color,original);
+        for(auto u:v){
+            int i=u.first;
+            int j=u.second;
+            grid[i][j]=original;
         }
         for(int i=0;i<grid.size();i++){
             for(int j=0;j<grid[0].size();j++){
