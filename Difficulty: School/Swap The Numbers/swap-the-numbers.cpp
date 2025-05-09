7cb8f62c51843f5ddbf95c0ce38784994d82b28a
Vedant Patil <157149251+VedantPatil3110@users.noreply.github.com>
2025-05-09 17:43:47 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: School/Swap The Numbers/swap-the-numbers.cpp
@@ -0,0 +1,19 @@
+//{ Driver Code Starts
+
+// } Driver Code Ends
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    swap(a,b);
+
+    cout << a << " " << b << endl;
+    return 0;
+}
+
+
+//{ Driver Code Starts.
+// } Driver Code Ends
