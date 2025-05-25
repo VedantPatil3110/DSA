8ac86d4f9f1c559b067de179fa659d59b9e74c35
Vedant Patil <157149251+VedantPatil3110@users.noreply.github.com>
2025-05-25 14:18:18 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (97.42%) - LeetSync

---
+++ 338-counting-bits/counting-bits.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int>dp(n+1);
+        int reset=1;
+        for(int i=1;i<=n;i++){
+            if(reset*2==i){
+                reset=i;
+            }
+            dp[i]=dp[i-reset]+1;
+        }
+        return dp;
+    }
+};
