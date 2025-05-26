f3244bb3f8373f4cbc85a840b2d8e1c4a486ecd3
Vedant Patil <157149251+VedantPatil3110@users.noreply.github.com>
2025-05-26 22:33:53 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Count number of hops/count-number-of-hops.cpp
@@ -0,0 +1,20 @@
+
+class Solution {
+  public:
+  int solve(int i,int n,vector<int>&dp){
+      if(i==n){
+          return 1;
+      }
+      if(i>n){
+          return 0;
+      }
+      if(dp[i]!=-1){
+          return dp[i];
+      }
+      return dp[i]=solve(i+1,n,dp)+solve(i+2,n,dp)+solve(i+3,n,dp);
+  }
+    int countWays(int n) {
+        vector<int>dp(n+1,-1);
+        return solve(0,n,dp);
+    }
+};
