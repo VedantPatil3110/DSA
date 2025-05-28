dc864b0e20ce4cdee9e920e234285c7c5391c708
Vedant Patil <157149251+VedantPatil3110@users.noreply.github.com>
2025-05-28 23:51:45 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Check if Arrays are Permutations/check-if-arrays-are-permutations.cpp
@@ -0,0 +1,8 @@
+// User function Template for C++
+
+// Complete this function
+bool isArr2PermutationOfArr1(vector<int>& arr1, vector<int>& arr2) {
+    int n=arr1.size()-1;
+    return is_permutation(arr1.begin(), arr1.end(), arr2.begin());
+    
+}
