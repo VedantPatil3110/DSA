59187d0595734cec10f8c322ad56cf2db72762ad
Vedant Patil <157149251+VedantPatil3110@users.noreply.github.com>
2025-09-04 08:41:16 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (55.11%) - LeetSync

---
+++ 3830-find-closest-person/find-closest-person.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int findClosest(int x, int y, int z) {
+        int a=abs(z-x);
+        int b=abs(z-y);
+        if(a<b){
+            return 1;
+        }
+        if(a>b){
+            return 2;
+        }
+        return 0;
+    }
+};
