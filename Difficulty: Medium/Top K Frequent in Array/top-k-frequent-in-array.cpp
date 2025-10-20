a6d9d7f27769c02479b93697a8764f674ed85d61
Vedant Patil <157149251+VedantPatil3110@users.noreply.github.com>
2025-10-21 02:39:10 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Top K Frequent in Array/top-k-frequent-in-array.cpp
+++ Difficulty: Medium/Top K Frequent in Array/top-k-frequent-in-array.cpp
@@ -1,64 +1,19 @@
-//{ Driver Code Starts
-// Initial Template for C++
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
 class Solution {
   public:
-    vector<int> topKFrequent(vector<int> &arr, int k) {
-         map<int,int>m;
-        for(auto x:arr){
-            m[x]++;
+    vector<int> topKFreq(vector<int> &arr, int k) {
+        map<int,int>mp;
+        for(int i=0;i<arr.size();i++){
+            mp[arr[i]]++;
         }
-        priority_queue<pair<int,int>>p;
-        for(auto x:m){
-            p.push({x.second,x.first});
+        priority_queue<pair<int,int>>pq;
+        for(auto x:mp){
+            pq.push({x.second,x.first});
         }
-        vector<int>ans(k);
-        for(int i=0;i<k;i++){
-            ans[i]=p.top().second;
-            p.pop();
+        vector<int>ans;
+        while(k--){
+            ans.push_back(pq.top().second);
+            pq.pop();
         }
         return ans;
     }
 };
-
-
-
-//{ Driver Code Starts.
-
-int main() {
-
-    int t;
-    cin >> t;
-    cin.ignore();
-
-    while (t--) {
-
-        string ks;
-        getline(cin, ks);
-        int k = stoi(ks);
-        vector<int> arr;
-        string input;
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-        Solution obj;
-        vector<int> res = obj.topKFrequent(arr, k);
-        for (int i = 0; i < res.size(); i++)
-            cout << res[i] << " ";
-        cout << endl;
-        cout << "~"
-             << "\n";
-    }
-
-    return 0;
-}
-
-// } Driver Code Ends
