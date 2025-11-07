0129b988a892723e559502d094493f46fe948528
Vedant Patil <157149251+VedantPatil3110@users.noreply.github.com>
2025-11-07 18:49:13 +0530

    Time: 0 ms (100.00%) | Memory: 18.3 MB (44.90%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -1,23 +1,19 @@
 class Solution {
 public:
-void dfs(vector<vector<int>>&image,int currrow,int currcoll,int initialcolor,int newcolor){
-    int n=image.size();
-    int m=image[0].size();
-    if(currrow<0 || currcoll<0 || currrow>=n || currcoll>=m){
+vector<vector<int>>dir{{1,0},{-1,0},{0,1},{0,-1}};
+void dfs(vector<vector<int>>&grid,int i,int j,int color,int newcolor){
+    if(i>=grid.size() || i<0 || j>=grid[0].size() || j<0 || grid[i][j]!=color){
         return;
     }
-    if(image[currrow][currcoll]!=initialcolor){
-        return;
+    grid[i][j]=newcolor;
+    for(int d=0;d<4;d++){
+        int ni=i+dir[d][0];
+        int nj=j+dir[d][1];
+        dfs(grid,ni,nj,color,newcolor);
     }
-    image[currrow][currcoll]=newcolor;
-    dfs(image,currrow+1,currcoll,initialcolor,newcolor);
-    dfs(image,currrow,currcoll-1,initialcolor,newcolor);
-    dfs(image,currrow-1,currcoll,initialcolor,newcolor);
-    dfs(image,currrow,currcoll+1,initialcolor,newcolor);
-
 }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        if(color==image[sr][sc]){
+        if(image[sr][sc]==color){
             return image;
         }
         dfs(image,sr,sc,image[sr][sc],color);
