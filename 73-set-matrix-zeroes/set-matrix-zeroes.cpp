5e06e89fa3803906550ce4d9285c120a01cb3340
Vedant Patil <157149251+VedantPatil3110@users.noreply.github.com>
2026-01-14 20:54:48 +0530

    Time: 0 ms (100.00%) | Memory: 18.7 MB (22.07%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,24 +1,30 @@
 class Solution {
 public:
+vector<vector<bool>>flag;
+void freaky(int i,int j,int m,int n,vector<vector<int>>&matrix){
+    flag[i][j]=false;
+    for(int k=0;k<m;k++){
+        if(matrix[k][j]!=0 && flag[k][j]==true){
+            flag[k][j]=false;
+        }
+        matrix[k][j]=0;
+    }
+    for(int k=0;k<n;k++){
+        if(matrix[i][k]!=0 && flag[i][k]==true){
+            flag[i][k]=false;
+        }
+        matrix[i][k]=0;
+    }
+}
     void setZeroes(vector<vector<int>>& matrix) {
-        set<int>row;
-        set<int>col;
-        for(int i=0;i<matrix.size();i++){
-            for(int j=0;j<matrix[0].size();j++){
-                if(matrix[i][j]==0){
-                    row.insert(i);
-                    col.insert(j);
+        int m=matrix.size();
+        int n=matrix[0].size();
+        flag.resize(m,vector<bool>(n,true));
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(matrix[i][j]==0 && flag[i][j]==true){
+                    freaky(i,j,m,n,matrix);
                 }
-            }
-        }
-        for(auto x:row){
-            for(int i=0;i<matrix[0].size();i++){
-                matrix[x][i]=0;
-            }
-        }
-        for(auto x:col){
-            for(int i=0;i<matrix.size();i++){
-                matrix[i][x]=0;
             }
         }
     }
