66782e55f3ebd74b5e476dfbf39a8350aed2a8fa
Vedant Patil <157149251+VedantPatil3110@users.noreply.github.com>
2026-02-03 01:48:40 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Insertion Sort/insertion-sort.cpp
+++ Difficulty: Easy/Insertion Sort/insertion-sort.cpp
@@ -1,15 +1,14 @@
 class Solution {
   public:
     void insertionSort(vector<int>& arr) {
-        int n=arr.size();
-        for(int i=1;i<n;i++){
-            int k=arr[i];
+        for(int i=1;i<arr.size();i++){
+            int freaky=arr[i];
             int j=i-1;
-            while(j>=0 && arr[j]>k){
+            while(j>=0 && arr[j]>freaky){
                 arr[j+1]=arr[j];
-                j-=1;
+                j--;
             }
-            arr[j+1]=k;
+            arr[j+1]=freaky;
         }
     }
 };
