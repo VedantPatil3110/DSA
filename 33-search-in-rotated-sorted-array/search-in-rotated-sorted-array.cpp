d6eff2d8185477044eac6d8f47e17bae0e3d39af
Vedant Patil <157149251+VedantPatil3110@users.noreply.github.com>
2026-02-23 16:51:40 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (95.53%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,76 +1,72 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-        int n=nums.size();
+    int search(vector<int>& arr, int target) {
+        int n=arr.size();
         int i=0;
         int j=n-1;
         int piviot=-1;
         while(i<=j){
-            int mid=i+((j-i)/2);
-            if(mid<n-1 && nums[mid]>nums[mid+1]){
+            int mid=i+(j-i)/2;
+            if(mid<n-1 && arr[mid]>arr[mid+1]){
                 piviot=mid;
                 break;
             }
-            if(mid>0 && nums[mid] < nums[mid-1]){
-                piviot = mid - 1;
+            if(mid>0 && arr[mid]<arr[mid-1]){
+                piviot=mid-1;
                 break;
             }
-            if (nums[mid] >= nums[i]) {
-                i = mid + 1;
-            } 
-            else {
-                j = mid-1;
+            if(arr[mid]>=arr[i]){
+                i=mid+1;
+            }
+            else{
+                j=mid-1;
             }
         }
         if(piviot==-1){
             int lo=0;
             int hi=n-1;
             while(lo<=hi){
-                int mid=lo+((hi-lo)/2);
-                if(nums[mid]==target){
+                int mid=lo+(hi-lo)/2;
+                if(arr[mid]==target){
                     return mid;
                 }
-                else if(target<nums[mid]){
-                    hi=mid-1;
+                else if(arr[mid]<target){
+                    lo=mid+1;
                 }
                 else{
+                    hi=mid-1;
+                }
+            }
+        }
+        else if(target>=arr[0] && target<=arr[piviot]){
+            int lo=0;
+            int hi=piviot;
+            while(lo<=hi){
+                int mid=lo+(hi-lo)/2;
+                if(arr[mid]==target){
+                    return mid;
+                }
+                else if(arr[mid]<target){
                     lo=mid+1;
+                }
+                else{
+                    hi=mid-1;
                 }
             }
         }
         else{
-            if(nums[0]<=target && nums[piviot]>=target){
-                int lo=0;
-                int hi=piviot;
-                while(lo<=hi){
-                    int mid=lo+((hi-lo)/2);
-                    if(nums[mid]==target){
-                        return mid;
-                        break;
-                    }
-                    else if(target<nums[mid]){
-                        hi=mid-1;
-                    }
-                    else{
-                        lo=mid+1;
-                    }
+            int lo=piviot+1;
+            int hi=n-1;
+            while(lo<=hi){
+                int mid=lo+(hi-lo)/2;
+                if(arr[mid]==target){
+                    return mid;
                 }
-            }
-            else{
-                int lo=piviot+1;
-                int hi=n-1;
-                while(lo<=hi){
-                    int mid=lo+((hi-lo)/2);
-                    if(nums[mid]==target){
-                        return mid;
-                        break;
-                    }
-                    else if(target<nums[mid]){
-                        hi=mid-1;
-                    }
-                    else{
-                        lo=mid+1;
-                    }
+                else if(arr[mid]<target){
+                    lo=mid+1;
+                }
+                else{
+                    hi=mid-1;
                 }
             }
         }
