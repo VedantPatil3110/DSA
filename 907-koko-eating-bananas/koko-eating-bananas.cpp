0c956f241e214be6b51301b9d0027373e030c173
Vedant Patil <157149251+VedantPatil3110@users.noreply.github.com>
2026-03-05 12:48:04 +0530

    Time: 27 ms (5.51%) | Memory: 23 MB (12.19%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -1,29 +1,25 @@
 class Solution {
 public:
-long long poss(vector<int>&piles,int mid){
-    long long  x=0;
+long long freaky(vector<int>&piles,int x){
+    long long ans=0;
     for(int i=0;i<piles.size();i++){
-        x+=ceil((double)piles[i]/mid);
+        ans=ans+ceil(double(piles[i])/x);
     }
-    return x;
+    return ans;
 }
     int minEatingSpeed(vector<int>& piles, int h) {
-        int maxi=INT_MIN;
-        for(int i=0;i<piles.size();i++){
-            maxi=max(piles[i],maxi);
-        }
-        int lo=1;
-        int ho=maxi;
-        int ans=maxi;
-        while(lo<=ho){
-            int mid = lo + (ho - lo) / 2;
-            long long time=poss(piles,mid);
-            if(time<=h){
-                ans=min(ans,mid);
-                ho=mid-1;
+        int i=1;
+        int j=*max_element(piles.begin(),piles.end());
+        int ans=0;
+        while(i<=j){
+            int mid=i+(j-i)/2;
+            long long cal=freaky(piles,mid);
+            if(cal<=h){
+                ans=mid;
+                j=mid-1;
             }
             else{
-                lo=mid+1;
+                i=mid+1;
             }
         }
         return ans;
