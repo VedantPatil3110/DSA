46ef7114d893d98429d94c52c3a3f7b96f3530d6
Vedant Patil <157149251+VedantPatil3110@users.noreply.github.com>
2026-03-17 23:50:48 +0530

    Time: 8 ms (7.31%) | Memory: 9.6 MB (91.25%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -4,43 +4,13 @@ public:
         if(s.size()!=t.size()){
             return false;
         }
-        unordered_map<char,int>m1;
-        // unordered_map<char,int>m2;
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
         for(int i=0;i<s.size();i++){
-            m1[s[i]]++;
-            // m2[t[i]]++;
-        }
-        // for(auto x:m1){
-        //     char ch1=x.first;
-        //     int freq1=x.second;
-        //     if(m2.find(ch1)!=m2.end()){
-        //         int freq2=m2[ch1];
-        //         if(freq1!=freq2){
-        //             return false;
-        //         }
-        //     }
-        //     else{
-        //         return false;
-        //     }
-        // }
-        // return true;
-        for(int i=0;i<s.size();i++){
-            char ch=t[i];
-            if(m1.find(ch)!=m1.end()){
-                m1[ch]--;
-                if(m1[ch]==0){
-                    m1.erase(ch);
-                }
-            }
-            else{
+            if(s[i]!=t[i]){
                 return false;
             }
         }
-        if(m1.size()>0){
-            return false;
-        }
-        else{
-            return true;
-        }
+        return true;
     }
 };
