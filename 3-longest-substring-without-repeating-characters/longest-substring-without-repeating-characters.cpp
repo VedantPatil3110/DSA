4f5750f5374f665d4680816d88211dd084879867
Vedant Patil <157149251+VedantPatil3110@users.noreply.github.com>
2026-03-24 09:22:24 +0530

    Time: 19 ms (28.40%) | Memory: 17 MB (7.84%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -4,17 +4,17 @@ public:
         if(s.size()==0){
             return 0;
         }
-        unordered_set<char>st;
         int left=0;
-        int mcount=INT_MIN;
+        int ans=INT_MIN;
+        set<char>st;
         for(int i=0;i<s.size();i++){
             while(st.find(s[i])!=st.end()){
                 st.erase(s[left]);
                 left++;
             }
             st.insert(s[i]);
-            mcount=max(mcount,i-left+1);
+            ans=max(ans,i-left+1);
         }
-        return mcount;
+        return ans;
     }
 };
