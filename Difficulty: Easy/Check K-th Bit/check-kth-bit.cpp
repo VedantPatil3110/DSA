ffd854a9627ae91e29e1dee3aa5ea249872997f6
Vedant Patil <157149251+VedantPatil3110@users.noreply.github.com>
2026-03-25 17:49:07 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Check K-th Bit/check-kth-bit.cpp
@@ -0,0 +1,11 @@
+class Solution {
+  public:
+    bool checkKthBit(int n, int k) {
+        if(((n>>k)&1)==1){
+            return true;
+        }
+        else{
+            return 0;
+        }
+    }
+};
